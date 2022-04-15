a742d11858586a69b83409ae6e7f135d210f3df1
Andrii Buchinskiy <buchinskia@gmail.com>
2022-04-15 13:31:01 +0300

    start

---
+++ db.php
@@ -0,0 +1,10 @@
+<?php
+$host = 'localhost';
+$dbname = 'User_list';
+$username = 'admin1';
+$password = 'Tesla234';
+
+$conn = mysqli_connect($host,$username,$password,$dbname);
+
+
+
---
+++ pdoconfig.php
@@ -0,0 +1,5 @@
+<?php
+$host = 'localhost';
+$dbname = 'Groups';
+$username = 'admin1';
+$password = 'Tesla234';
